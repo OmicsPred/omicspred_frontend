b1dd8f7ac11e22418c722d448b4d0b2cb4eb38d2
Laurent Gil <lg10@sanger.ac.uk>
2023-10-20 09:34:58 +0100

    Add .env files

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), pluginRewriteAll()],
-  server: {
-    port: 3000,
-    hot: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env': env
+    },
+    // define: {
+    //   'process.env.REACT_APP_DOI': JSON.stringify(env.REACT_APP_DOI)
+    // },
+    plugins: [react(), pluginRewriteAll()],
+    server: {
+      port: 3000,
+      hot: true
+    }
   }
 })
