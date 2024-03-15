fa1fb832eb7be1976899aecdad17e0f9e74b5aa5
Laurent Gil <lg10@sanger.ac.uk>
2024-03-15 14:30:43 +0000

    Change a bit the file structure and add Pathways

---
+++ src/pages/MolecularTrait/components/omics.jsx
@@ -0,0 +1,43 @@
+import Href from '../../../components/Href';
+
+
+const display_omics_link = (data, type, index) => {
+    if (data.external_id) {
+        return <span key={type+'_'+data.external_id}>{index ? ', ': ''}{data.name ? data.name+' ' : ''}(<Href href={'/'+type+'/'+data.external_id} key={'link'+data.external_id} text={data.external_id}/>)</span>
+    }
+    else {
+        return <span key={type+'_'+data.name}>{index ? ', ': ''}<Href href={'/'+type+'/'+data.name} key={'link'+data.name} text={data.name}/></span>
+    }
+}
+
+export const display_gene_link = (gene, index) => {
+    return display_omics_link(gene,'gene',index)
+}
+
+export const display_protein_link = (protein, index) => {
+    return display_omics_link(protein,'protein',index)
+}
+
+export const display_metabolite_link = (metabolite, index) => {
+    return display_omics_link(metabolite,'metabolite',index)
+}
+
+export const display_synonyms = (synonyms) => {
+    let syn_lists = []
+    if (synonyms) {
+        for (let i=0; i<synonyms.length; i++) {
+            syn_lists.push(synonyms[i].name);
+        }
+    }
+    return syn_lists.join(', ');
+}
+
+export const display_xrefs = (xrefs) => {
+    let xref_lists = []
+    if (xrefs) {
+        for (let i=0; i<xrefs.length; i++) {
+            xref_lists.push(xrefs[i].name+' ['+xrefs[i].source+']');
+        }
+    }
+    return xref_lists.join(', ');
+}
--- src/pages/Omics/Metabolomics/index.jsx
+++ src/pages/Omics/Metabolomics/index.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import DataTableFromRestApi from "../../components/table/DataTableFromRestApi";
-import { metabolomics_columns, metabolomics_column_groups } from '../../components/table/columns/metabolomics'
-import restApiCall from '../../components/RestAPI';
+import DataTableFromRestApi from "../../../components/table/DataTableFromRestApi";
+import { metabolomics_columns, metabolomics_column_groups } from '../../../components/table/columns/metabolomics'
+import restApiCall from '../../../components/RestAPI';
 
 function Metabolomics() {
     let { platform } = useParams();
